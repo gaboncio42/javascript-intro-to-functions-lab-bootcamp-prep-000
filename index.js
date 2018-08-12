30f9a202fee6d9b2224a082d2edfd13e2beb11a9
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-08-12 21:47:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,4 @@ function logWhisper(string) {
   console.log('Hello'.toLowerCase());
 }
 
-var a = 'hello';
-var b = 'hello'.toUpperCase();
-var c = "I love you, Grandma.";
-
-function sayHiToGrandma(string) {
-  if (a) {
-    return I can't hear you!.
-  }
   
