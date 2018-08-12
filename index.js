b093c6418f342beffbd9beccfcf39d6569977199
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-08-12 22:00:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()===string) {
-    return "I can\'t hear you.";
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!";
   } if (string.toUpperCase() === string) {
     return "YES INDEED!";
   } if (string === "I love you, Grandma.") {
