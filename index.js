96533940f03aecf50a4cd757f779e12f0c966d73
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-08-12 21:01:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var a = 'hello';
 var b = 'hello'.toUpperCase();
 var c = "I love you, Grandma";
 
-function sayHiToGrandma(string) {
+function sayHiToGrandma(a, b, c) {
   if (a) {
     return "I can't hear you!";
   } else if (b) {
